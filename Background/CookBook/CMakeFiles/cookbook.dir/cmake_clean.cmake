b5bd833cee25767bb985f2bab7b86fdd1bbff98a
CJ.Yang <ascia1112@gmail.com>
2014-05-16 18:39:45 +0800

    Yeah

---
+++ Background/CookBook/CMakeFiles/cookbook.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cookbook.dir/cookbook.cpp.o"
+  "cookbook.pdb"
+  "cookbook"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cookbook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- Display/Rtsp/Rtsp.cpp
+++ Display/Rtsp/Rtsp.cpp
@@ -1,49 +1,51 @@
-#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include "opencv2/highgui/highgui_c.h"
+#include "opencv2/highgui.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
+#include "opencv2/imgproc/imgproc_c.h"
 #include <iostream>
+
 
 using namespace cv;
 using namespace std;
-
 int main(int argc, char* argv[])
 {
-    VideoCapture cap(argv[1]); // open the video file for reading
+    //cvlc -vvv rtsp://admin:123456@172.16.15.194:7070/track1 --sout="#std{access=file,mux=ps,dst=video}"
 
-    if ( !cap.isOpened() )  // if not success, exit program
+    CvCapture* capture = 0; 
+    capture = cvCaptureFromAVI(argv[1]);
+    if (!capture)  // if not success, exit program
     {
-         cout << "Cannot open the video file" << endl;
-         return -1;
+        cout << "Cannot open the video cam" << endl;
+        return -1;
     }
 
-    //cap.set(CV_CAP_PROP_POS_MSEC, 300); //start the video at 300ms
+   double dWidth = cvGetCaptureProperty( capture, CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
+   double dHeight = cvGetCaptureProperty( capture, CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
 
-    double fps = cap.get(CV_CAP_PROP_FPS); //get the frames per seconds of the video
-
-     cout << "Frame per seconds : " << fps << endl;
+    cout << "Frame size : " << dWidth << " x " << dHeight << endl;
 
     namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
+    namedWindow("MyNegativeVideo",CV_WINDOW_AUTOSIZE);
 
-    while(1)
+    while (1)
     {
         Mat frame;
-
-        bool bSuccess = cap.read(frame); // read a new frame from video
-
-         if (!bSuccess) //if not success, break loop
-        {
-                        cout << "Cannot read the frame from video file" << endl;
-                       break;
-        }
+        Mat contours;
+        IplImage* iplImg = cvQueryFrame( capture );  
+        //bool bSuccess = cap.read(frame); // read a new frame from video
+        frame = cv::cvarrToMat(iplImg);
+         
 
         imshow("MyVideo", frame); //show the frame in "MyVideo" window
 
-        if(waitKey(30) == 27) //wait for 'esc' key press for 30 ms. If 'esc' key is pressed, break loop
+        
+        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
        {
-                cout << "esc key is pressed by user" << endl; 
-                break; 
+            cout << "esc key is pressed by user" << endl;
+            break;
        }
     }
-
     return 0;
-
 }
